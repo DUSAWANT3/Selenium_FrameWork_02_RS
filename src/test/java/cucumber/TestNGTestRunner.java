59214800f6e863e736f0c717eac2371ea19bf79d
DUSAWANT3 <durgesh3230@gmail.com>
2025-05-17 19:56:32 +0530

    Selenium Framework 0517

--- src/test/java/cucumber/TestNGTestRunner.java
+++ src/test/java/cucumber/TestNGTestRunner.java
@@ -4,12 +4,12 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(
-        features = "src/test/java/cucumber",
-        glue = "org.dutesting.stepDefinitions",
-        monochrome = true,
-        plugin = {"html:target/cucumber.html"},
-        tags = "@ErrorValidation"
+        features = "src/test/java/cucumber",      // Path to your feature files
+        glue = "org.dutesting.stepDefinitions",   // Package containing step definitions class
+        monochrome = true,                         // Cleaner console output
+        plugin = {"html:target/cucumber.html"}      // Report generation
 )
+       //tags = "@ErrorValidation"                 // Uncomment to run specific tag
 public class TestNGTestRunner extends AbstractTestNGCucumberTests {
-
+    // Extending AbstractTestNGCucumberTests integrates Cucumber with TestNG
 }
