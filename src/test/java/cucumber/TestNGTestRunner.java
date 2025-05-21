cde24d34798c1890751729120ed04f18e737f3e4
DUSAWANT3 <durgesh3230@gmail.com>
2025-05-21 21:22:48 +0530

    Selenium Framework 0521

--- src/test/java/cucumber/TestNGTestRunner.java
+++ src/test/java/cucumber/TestNGTestRunner.java
@@ -12,4 +12,5 @@ import io.cucumber.testng.CucumberOptions;
        //tags = "@ErrorValidation"                 // Uncomment to run specific tag
 public class TestNGTestRunner extends AbstractTestNGCucumberTests {
     // Extending AbstractTestNGCucumberTests integrates Cucumber with TestNG
+
 }
